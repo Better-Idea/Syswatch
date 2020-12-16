ae3b6b83795e328d8c310ff4da18e1f03656dc87
PowerfulLeon <msm625@foxmail.com>
2020-12-16 20:59:09 +0800

    update include/sysconfig.h

--- include/sysconfig.h
+++ include/sysconfig.h
@@ -5,6 +5,8 @@
 #include<stddef.h>
 #include<stdint.h>
 
+#define SYSW_PORT                       8848
+#define SYSW_MAX_CONN                   128
 #define SYSW_MAX_CPU_NUM                256
 #define SYSW_MAX_NUMA_NUM               32
 #define SYSW_MAX_DISK_NUM               128
@@ -12,6 +14,9 @@
 
 #define SYSW_CPU_BMP_SIZE               \
     ((SYSW_MAX_CPU_NUM / (sizeof(size_t) * 8)) + SYSW_MAX_CPU_NUM % (sizeof(size_t) * 8) != 0)
+
+#define SYSW_NUMA_BMP_SIZE              \
+    ((SYSW_MAX_NUMA_NUM / (sizeof(size_t) * 8)) + SYSW_MAX_NUMA_NUM % (sizeof(size_t) * 8) != 0)
 
 
 #define SYSWATCH_DEFAULT_PATH_PERIOD    "syswatch.rate"
